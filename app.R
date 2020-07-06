782ac116823e44c20f9f6cb69c5342e2282697fc
Tony Gojanovic <tonygojanovic@Tonys-MacBook-Pro.local>
2020-07-05 23:12:46 -0600

    modified app

--- app.R
+++ app.R
@@ -12,22 +12,45 @@ ui <- fluidPage(
       numericInput('obj_fl', 'Focal Length in mm',1000,min=0,max=100000,step = 1),
       numericInput('eye_fl','Eyepiece focal length in mm',20,min=0,max=100,step=1), 
       numericInput("eye_fov",'Eyepiece field of view in degrees',50,min = 20,max=100,step=1),
-      sliderInput('eye_dia',"Pupil diameter of the observer",7,min=5,max=10,step=1),
+      sliderInput('pupil_dia',"Pupil diameter of the observer",7,min=4,max=8,step=0.5),
       submitButton('Calculate')
     ),
     mainPanel(withMathJax(),
               # Output summary
               h3("Instructions"),
-              p("Input the Input the telescope optic parameters. Press calculate and performance characteristics will be calculated"),
-              h4("Focal ratio:"),
-              verbatimTextOutput("focal_ratio"),
+              p("Input the Input the telescope optic parameters. Press calculate and performance characteristics will be calculated."),
+              strong("Note"),
+              p("Telescope performance is highly dependent on seeing and transparency conditions."),
+              h3("Eyepiece performance"),
               h4("Magnifcation:"),
               verbatimTextOutput("magnifcation"),
               h4("Field of view (degrees):"),
               verbatimTextOutput("telefov"),
-              h4("Resolving power (arcseconds):"),
-              verbatimTextOutput("dawes_limit")
-              
+              h4("Exit pupil diameter mm:"),
+              verbatimTextOutput("exit_pup"),
+              h4("Lowest useful magnification based on observer's pupil diameter:"),
+              verbatimTextOutput("min_mag"),
+              h3("Telescope performance"),
+              h4("Focal ratio:"),
+              verbatimTextOutput("focal_ratio"),
+              h4("Theoretical Resolving power (Dawes limit in arcseconds):"),
+              verbatimTextOutput("dawes_limit"),
+              h4("Gathering power (light grasp) compared to human eye for a given pupil diameter:"),
+              verbatimTextOutput("GL"),
+              h4("Approximate Limiting magnitude (under dark, moonless skies):"),
+              verbatimTextOutput("limit_mag"),
+
+              h4("Notes"),
+              strong("Suggested pupil diameters by age"),
+              tags$ul(
+                tags$li("20 years or less = 7.5 mm"),
+                tags$li("30 years = 7.0 mm"),
+                tags$li("35 years = 6.5 mm"),
+                tags$li("45 years = 6.0 mm"),
+                tags$li("60 years = 5.5 mm"),
+                tags$li("80 years = 5.0 mm")
+              ),
+              tags$a(href="http://www.rocketmime.com/astronomy/Telescope/telescope_eqn.html", "Click here for telescope math equations")
     )
   )
 )
@@ -42,23 +65,44 @@ ui <- fluidPage(
   # Determine the magnification
   mag<-function(obj_fl,eye_fl){
     magnification<-obj_fl/eye_fl
-    magnification <- round(magnification,1)
-    return(magnification)
+    return(round(magnification,1))
   }
   
   # Determine the field of view
   fov<-function(obj_fl,eye_fl,eye_fov){
     mag<-obj_fl/eye_fl
     field<-eye_fov/mag
-    field<-round(field,2)
-    return(field)
+    return(round(field,2))
   }
   
   # Determine the resolving power (Dawes Limit)
   dawes<-function(obj_dia){
     resolution <- 115.8/obj_dia
-    resolution<-round(resolution,2)
-    return(resolution)
+    return(round(resolution,2))
+  }
+  
+  # Determine the light graph or gathering power
+  light_grasp<-function(obj_dia,pupil_dia){
+    gather <- (obj_dia/pupil_dia)^2
+    return(round(gather,1))
+  } 
+  
+  # Determine limiting magnitude
+  lim_mag<-function(obj_dia){
+    limiting_mag<-2+5*(log10(obj_dia))
+    return(round(limiting_mag,2))
+  }
+  
+  # Determine exit pupil
+  ep <- function(obj_dia,eye_fl,obj_fl){
+    exit_pupil <- eye_fl/(obj_fl/obj_dia)
+    return(round(exit_pupil,1))
+  }
+  
+  # Determine minimum magnification
+  min_val <- function(obj_dia,pupil_dia){
+    min_magnification <- obj_dia/pupil_dia
+    return(round(min_magnification,1))
     
   }
     
@@ -67,6 +111,10 @@ server <- function(input, output) {
   output$magnifcation <- renderPrint({mag(input$obj_fl,input$eye_fl)})
   output$telefov <- renderPrint({fov(input$obj_fl,input$eye_fl,input$eye_fov)})
   output$dawes_limit <- renderPrint({dawes(input$obj_dia)})
+  output$GL <- renderPrint({light_grasp(input$obj_dia,input$pupil_dia)})
+  output$limit_mag <- renderPrint({lim_mag(input$obj_dia)})
+  output$exit_pup <- renderPrint({ep(input$obj_dia,input$eye_fl,input$obj_fl)})
+  output$min_mag <- renderPrint({min_val(input$obj_dia,input$pupil_dia)})
 }
 shinyApp(ui, server)
 
